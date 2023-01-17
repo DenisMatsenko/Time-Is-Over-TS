1fb4b8d85d09df55ac298b5f375b4b0a31d9b5c4
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2023-01-17 09:18:32 +0100

    Fixed Every day event

--- sup-functions/StartNotifyTimeChecker.ts
+++ sup-functions/StartNotifyTimeChecker.ts
@@ -1,16 +1,27 @@
 import { Client } from 'discord.js'
-import { hourForNotify } from '../settings/settings'
+import { hourForNotify, minutesForNotify } from '../settings/settings'
 import ClearChannel from './ClearChannel'
 import DelteteOldReminders from './DeleteOldReminders'
 import SendTestListToChannel from './SendTestListToChannel'
 
 export default async function StartEventTimeChecker(client: Client) {
-    setInterval(async () => {
-        if (new Date().getHours() == hourForNotify) {
-            await ClearChannel(client).then(() => StartSendNotify(client));
-        }
-    }, 1800000);
+    RunAtSpecificTimeOfDay(hourForNotify, minutesForNotify, async () => {await ClearChannel(client).then(() => StartSendNotify(client))})
 }
 
-const StartSendNotify = async (client: Client) => 
-    await DelteteOldReminders().then(() => {SendTestListToChannel(client)})   
+const RunAtSpecificTimeOfDay = (hour: number, minutes: number, func: Function) => {
+    const twentyFourHours = 86400000;
+    const now: any = new Date();
+    let eta_ms = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minutes, 0, 0).getTime() - now;
+    if (eta_ms < 0)
+    {
+      eta_ms += twentyFourHours;
+    }
+    setTimeout(function() {
+      //run once
+      func();
+      // run every 24 hours from now on
+      setInterval(func, twentyFourHours);
+    }, eta_ms);
+}
+
+const StartSendNotify = async (client: Client) => await DelteteOldReminders().finally(() => { SendTestListToChannel(client)})
